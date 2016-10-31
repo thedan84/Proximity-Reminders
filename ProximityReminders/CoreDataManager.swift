f34f765888e98a0ff8459f1c4da2fe4731d3102e
Dennis Parussini <thedan84@mac.com>
2016-10-31 08:16:24 +0100

    Basic setup

---
+++ ProximityReminders/CoreDataManager.swift
@@ -0,0 +1,41 @@
+//
+//  CoreDataManager.swift
+//  ProximityReminders
+//
+//  Created by Dennis Parussini on 31-10-16.
+//  Copyright © 2016 Dennis Parussini. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+class CoreDataManager {
+    
+    static let sharedManager = CoreDataManager()
+    
+    // MARK: - Core Data stack
+    
+    lazy var persistentContainer: NSPersistentContainer = {
+        let container = NSPersistentContainer(name: "ProximityReminders")
+        container.loadPersistentStores(completionHandler: { (storeDescription, error) in
+            if let error = error as NSError? {
+                fatalError("Unresolved error \(error), \(error.userInfo)")
+            }
+        })
+        return container
+    }()
+    
+    // MARK: - Core Data Saving support
+    
+    func saveContext () {
+        let context = persistentContainer.viewContext
+        if context.hasChanges {
+            do {
+                try context.save()
+            } catch {
+                let nserror = error as NSError
+                fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
+            }
+        }
+    }
+}
